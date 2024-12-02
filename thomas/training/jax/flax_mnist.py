371a80b07b34268f91eb4dbcdc0ff4c7509d25d1
umalesTT <umales@tenstorrent.com>
2024-12-02 16:30:33 +0000

    Decomposed the code and fixed some bugs. Added file for training related functions, and now everything is called from main.

--- thomas/training/jax/flax_mnist.py
+++ thomas/training/jax/flax_mnist.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
-#
-# SPDX-License-Identifier: Apache-2.0
 import numpy as np
 import jax
 import jax.numpy as jnp
@@ -11,7 +8,9 @@ import optax
 
 from flax import linen as nn
 from flax.training import train_state
-from flax.serialization import to_state_dict, msgpack_serialize, from_bytes
+from flax.serialization import (
+    to_state_dict, msgpack_serialize, from_bytes
+)
 
 from tensorflow import keras
 import wandb
@@ -21,9 +20,10 @@ from model import Models, MLP
 from utils import ExportSHLO
 from logg_it import init_wandb, log_metrics, save_checkpoint, load_checkpoint
 
+from train_functions import forward_pass, forward_and_compute_loss, func_optax_loss, compute_loss_and_backward_pass, update_params, train_step, eval_step, calculate_metrics_train, calculate_metrics_val, accumulate_metrics
 
 def load_mnist():
-
+        
     (train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
 
     train_images = train_images[..., None] / 255.0
@@ -35,15 +35,13 @@ def load_mnist():
 
     # Split the training data into training and validation sets
     train_size = int(0.8 * len(train_images))
-    val_size = int(0.1 * len(train_images))
+    val_size = int(0.2 * len(train_images))
 
-    train_images, val_images = train_images[:train_size], train_images[train_size : train_size + val_size]
-    train_labels, val_labels = train_labels[:train_size], train_labels[train_size : train_size + val_size]
+    train_images, val_images = train_images[:train_size], train_images[train_size:train_size + val_size]
+    train_labels, val_labels = train_labels[:train_size], train_labels[train_size:train_size + val_size]
 
     return train_images, train_labels, val_images, val_labels, test_images, test_labels
 
-
-train_images, train_labels, eval_images, eval_labels, test_images, test_labels = load_mnist()
 
 
 class EarlyStopping:
@@ -60,166 +58,101 @@ class EarlyStopping:
             self.counter += 1
         return self.counter >= self.patience
 
+def train(get_best_checkpoint=False, use_export_shlo = False):
 
-early_stopping = EarlyStopping(patience=1)
+    train_images, train_labels, eval_images, eval_labels, test_images, test_labels = load_mnist()
 
-config = init_wandb(
-    project_name="Flax mnist mlp training", job_type="Flax mnist mlp training", dir_path="/proj_sw/user_dev/umales"
-)
+    config = init_wandb(project_name="Flax mnist mlp training", 
+                        job_type="Flax mnist mlp training", 
+                        dir_path='/proj_sw/user_dev/umales')
 
-config.learning_rate = 1e-3
-config.batch_size = 64
-config.num_epochs = 15
-config.seed = 0
-
-
-@jax.jit
-def forward_pass(params, x):
-    def apply_fn(x):
-        return MLP().apply({"params": params}, x, mutable=["params"])
-
-    return apply_fn(x)
+    config.learning_rate = 1e-3
+    config.batch_size = 64
+    config.num_epochs = 30
+    config.seed = 0
 
 
-def compute_loss(params, x, y):
-    logits, new_model_state = forward_pass(params, x)
-    loss = func_optax_loss(logits, y)
-    return loss, new_model_state
+    rng = random.PRNGKey(config.seed)
+    input_shape = (1, 28, 28, 1)
+    output_shape = jnp.ones((1, 10))
+    pred_model = Models(model_type='MLP')
+    params = pred_model.model.init(rng, jnp.ones(input_shape))['params']
+    tx = optax.sgd(learning_rate=config.learning_rate)
+    state = train_state.TrainState.create(apply_fn=pred_model.model.apply, params=params, tx=tx)
+
+    num_batches = len(train_images) // config.batch_size
+    num_eval_batches = len(eval_images) // config.batch_size
+
+    best_epoch = 0
+    #early_stopping = EarlyStopping(patience=1)
+    best_val_loss = 1e7
+    for epoch in range(config.num_epochs):
+
+        train_batch_metrics = []
+        for i in range(num_batches):
+            batch_images = train_images[i*config.batch_size:(i+1)*config.batch_size]
+            batch_labels = train_labels[i*config.batch_size:(i+1)*config.batch_size]
+            state, loss, grads = train_step(state,  batch_images, batch_labels)
+            
+            logits = eval_step(state.params, batch_images)
+            metrics = calculate_metrics_train(logits, batch_labels, loss)
+            train_batch_metrics.append(metrics)
+        train_batch_metrics_avg = accumulate_metrics(train_batch_metrics)
+
+        eval_batch_metrics = []
+        for i in range(num_eval_batches):
+            batch_images = eval_images[i*config.batch_size:(i+1)*config.batch_size]
+            batch_labels = eval_labels[i*config.batch_size:(i+1)*config.batch_size]
+            logits = eval_step(state.params, batch_images)
+            metrics = calculate_metrics_val(logits, batch_labels)
+            eval_batch_metrics.append(metrics)
+        eval_batch_metrics_avg = accumulate_metrics(eval_batch_metrics)
+
+        if(eval_batch_metrics_avg['loss'] < best_val_loss):
+            best_val_loss = eval_batch_metrics_avg['loss']
+            best_epoch = epoch
+
+        log_metrics(grads, state, train_batch_metrics_avg['loss'], train_batch_metrics_avg['accuracy'], eval_batch_metrics_avg['loss'], eval_batch_metrics_avg['accuracy'], epoch)
+        
+        base_checkpoint_dir = f"/proj_sw/user_dev/umales/checkpoints/{wandb.run.name}"
+        epoch_dir = f"epoch={epoch:02d}"
+        checkpoint_dir = os.path.join(base_checkpoint_dir, epoch_dir)
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        checkpoint_file_name = "checkpoint.msgpack"
+        checkpoint_file_path = os.path.join(checkpoint_dir, checkpoint_file_name)
+        save_checkpoint(checkpoint_file_path, state, epoch)
+        
+    if get_best_checkpoint:
+        # For some reason, wandb is unable to load 2 most recent checkpoints
+        # So, we load the best checkpoint and the two checkpoints before it
+        # I suppose this is a bug in wandb, some sort of latency between saving and loading
+        # becaue this pattern repeats no matter the number of epochs
+        epoch = best_epoch - 2
+        ckpt_file = "checkpoint.msgpack"
+        restored_state = load_checkpoint(ckpt_file, state, epoch)
+        logits = eval_step(restored_state.params, test_images)
+        metrics = calculate_metrics_val(logits, test_labels)
+        test_batch_metrics = []
+        test_batch_metrics.append(metrics)
+        test_batch_metrics_avg = accumulate_metrics(test_batch_metrics)
+        wandb.log({"Test Loss": test_batch_metrics_avg["loss"], "Test Accuracy": test_batch_metrics_avg["accuracy"]})
+    
+    wandb.finish()
 
 
-@jax.jit
-def func_optax_loss(logits, labels):
-    # one_hot_labels = jax.nn.one_hot(labels, num_classes=logits.shape[-1]).astype(jnp.float32)
-    return optax.losses.softmax_cross_entropy_with_integer_labels(logits=logits, labels=labels).mean()
+    if use_export_shlo:
 
+        from utils import ExportSHLO
 
-@jax.jit
-def backward_pass(params, x, y):
-    grad_fn = jax.value_and_grad(compute_loss, has_aux=True)
-    (loss, new_model_state), grads = grad_fn(params, x, y)
-    return grads, loss, new_model_state
+        export_it = ExportSHLO()
+        #export_it.export_fwd_train_to_StableHLO_and_get_ops(forward_pass, state, input_shape, print_stablehlo=False)
+        #export_it.export_fwd_tst_to_StableHLO_and_get_ops(eval_step, state, input_shape, print_stablehlo=False)
+        #export_it.export_bwd_to_StableHLO_and_get_ops(backward_pass, state, input_shape, print_stablehlo=False)
+        export_it.export_loss_to_StableHLO_and_get_ops(func_optax_loss, output_shape, print_stablehlo=False)
+        #export_it.export_optimizer_to_StableHLO_and_get_ops(update_params, state, grads, print_stablehlo=False)
 
+def main():
+    train(get_best_checkpoint=True, use_export_shlo=True)
 
-@jax.jit
-def update_params(state, grads):
-    return state.apply_gradients(grads=grads)
-
-
-@jax.jit
-def train_step(state, x, y):
-    grads, loss, new_model_state = backward_pass(state.params, x, y)
-    state = update_params(state, grads)
-    return state, new_model_state, loss, grads
-
-
-@jax.jit
-def eval_step(params, x):
-    logits, _ = forward_pass(params, x)
-    return logits
-
-
-@jax.jit
-def calculate_metrics(logits, y):
-    loss = func_optax_loss(logits, y)
-    accuracy = jnp.mean(jnp.argmax(logits, -1) == y)
-    metrics = {
-        "loss": loss,
-        "accuracy": accuracy,
-    }
-    return metrics
-
-
-def accumulate_metrics(metrics):
-    keys = metrics[0].keys()
-
-    if any(set(metric.keys()) != set(keys) for metric in metrics):
-        raise ValueError("All dictionaries in 'metrics' must have the same keys.")
-
-    count = len(metrics)
-    sums = {}
-    # Calculate the mean of each key (loss, accuracy, etc.)
-    for d in metrics:
-        for key, value in d.items():
-            if key not in sums:
-                sums[key] = 0
-            sums[key] += value
-
-    averages = {key: sums[key] / count for key in sums}
-    return averages
-
-
-rng = random.PRNGKey(config.seed)
-input_shape = (1, 28, 28, 1)
-output_shape = jnp.ones((1, 10))
-pred_model = Models(model_type="MLP")
-params = pred_model.model.init(rng, jnp.ones(input_shape))["params"]
-tx = optax.sgd(learning_rate=config.learning_rate)
-state = train_state.TrainState.create(apply_fn=pred_model.model.apply, params=params, tx=tx)
-
-batch_num = 0
-num_batches = len(train_images) // config.batch_size
-num_eval_batches = len(eval_images) // config.batch_size
-for epoch in range(config.num_epochs):
-
-    best_eval_loss = 1e3
-
-    train_batch_metrics = []
-    for i in range(num_batches):
-        batch_images = train_images[i * config.batch_size : (i + 1) * config.batch_size]
-        batch_labels = train_labels[i * config.batch_size : (i + 1) * config.batch_size]
-        state, _, loss, grads = train_step(state, batch_images, batch_labels)
-        batch_num = batch_num + 1
-
-        logits = eval_step(state.params, batch_images)
-        metrics = calculate_metrics(logits, batch_labels)
-        train_batch_metrics.append(metrics)
-    train_batch_metrics_avg = accumulate_metrics(train_batch_metrics)
-
-    eval_batch_metrics = []
-    for i in range(num_eval_batches):
-        batch_images = eval_images[i * config.batch_size : (i + 1) * config.batch_size]
-        batch_labels = eval_labels[i * config.batch_size : (i + 1) * config.batch_size]
-        logits = eval_step(state.params, batch_images)
-        metrics = calculate_metrics(logits, batch_labels)
-        eval_batch_metrics.append(metrics)
-    eval_batch_metrics_avg = accumulate_metrics(eval_batch_metrics)
-
-    log_metrics(
-        grads,
-        state,
-        train_batch_metrics_avg["loss"],
-        train_batch_metrics_avg["accuracy"],
-        eval_batch_metrics_avg["loss"],
-        eval_batch_metrics_avg["accuracy"],
-        epoch,
-    )
-
-    base_checkpoint_dir = f"/proj_sw/user_dev/umales/checkpoints/{wandb.run.name}"
-    epoch_dir = f"epoch={epoch:02d}"
-    checkpoint_dir = os.path.join(base_checkpoint_dir, epoch_dir)
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    checkpoint_file_name = "checkpoint.msgpack"
-    checkpoint_file_path = os.path.join(checkpoint_dir, checkpoint_file_name)
-    save_checkpoint(checkpoint_file_path, state, epoch)
-
-
-epoch = 0
-ckpt_file = "checkpoint.msgpack"
-restored_state = load_checkpoint(ckpt_file, state, epoch)
-logits = eval_step(restored_state.params, test_images)
-metrics = calculate_metrics(logits, test_labels)
-test_batch_metrics = []
-test_batch_metrics.append(metrics)
-test_batch_metrics_avg = accumulate_metrics(test_batch_metrics)
-wandb.log({"Test Loss": test_batch_metrics_avg["loss"], "Test Accuracy": test_batch_metrics_avg["accuracy"]})
-
-wandb.finish()
-
-# from utils import ExportSHLO
-
-export_it = ExportSHLO()
-# export_it.export_fwd_train_to_StableHLO_and_get_ops(forward_pass, state, input_shape, print_stablehlo=False)
-# export_it.export_fwd_tst_to_StableHLO_and_get_ops(eval_step, state, input_shape, print_stablehlo=False)
-# export_it.export_bwd_to_StableHLO_and_get_ops(backward_pass, state, input_shape, print_stablehlo=False)
-export_it.export_loss_to_StableHLO_and_get_ops(func_optax_loss, output_shape, print_stablehlo=False)
-# export_it.export_optimizer_to_StableHLO_and_get_ops(update_params, state, grads, print_stablehlo=False)
+if __name__ == "__main__":
+    main()
--- thomas/training/jax/logg_it.py
+++ thomas/training/jax/logg_it.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
-#
-# SPDX-License-Identifier: Apache-2.0
 import numpy as np
 import jax
 import jax.numpy as jnp
@@ -10,57 +7,48 @@ import os
 
 from flax.serialization import msgpack_serialize, from_bytes, to_state_dict
 
-
 def init_wandb(project_name, job_type, dir_path):
     wandb.init(dir=dir_path, project=project_name, job_type=job_type)
     config = wandb.config
     return config
 
-
 def log_metrics(grads, state, train_loss, train_accuracy, val_loss, val_accuracy, epoch, show_optimizer=False):
 
     for k, v in grads.items():
-        wandb.log({f"Gradients/{k}_bias": wandb.Histogram(v["bias"].flatten(), num_bins=100)}, step=epoch)
-        wandb.log({f"Gradients/{k}_kernel": wandb.Histogram(v["kernel"].flatten(), num_bins=100)}, step=epoch)
+        wandb.log({f"Gradients/{k}_bias": wandb.Histogram(v['bias'].flatten(), num_bins=100)}, step=epoch)
+        wandb.log({f"Gradients/{k}_kernel": wandb.Histogram(v['kernel'].flatten(), num_bins=100)}, step=epoch)
 
     for layer_name, layer_params in state.params.items():
         for param_name, param_values in layer_params.items():
-            wandb.log(
-                {f"Weights/{layer_name}_{param_name}": wandb.Histogram(param_values.flatten(), num_bins=100)},
-                step=epoch,
-            )
-
-    if show_optimizer:
+            wandb.log({f"Weights/{layer_name}_{param_name}": wandb.Histogram(param_values.flatten(), num_bins=100)}, step=epoch)
+    
+    if(show_optimizer):
         log_optimizer_state(state.opt_state, state.params, epoch)
 
-    wandb.log(
-        {
-            "train_loss": train_loss,
-            "train_accuracy": train_accuracy,
-            "val_loss": val_loss,
-            "val_accuracy": val_accuracy,
-        }
-    )
+    wandb.log({
+        "train_loss": train_loss,
+        "train_accuracy": train_accuracy,
+        "val_loss": val_loss,
+        "val_accuracy": val_accuracy,
+    })
 
-
-def log_optimizer_state(opt_state, params, epoch, prefix=""):
-    index_to_name = {1: "first_momentum", 2: "second_momentum"}
-
-    if isinstance(opt_state, dict):
-        for k, v in opt_state.items():
-            new_prefix = f"{prefix}/{k}" if prefix else k
-            log_optimizer_state(v, params.get(k, {}), new_prefix)
-    elif isinstance(opt_state, (list, tuple)):
-        for idx, v in enumerate(opt_state):
-            if idx in index_to_name:
-                name = index_to_name[idx]
-                new_prefix = f"{prefix}/{name}" if prefix else name
-                log_optimizer_state(v, params, new_prefix)
-            else:
-                log_optimizer_state(v, params, prefix)
-    elif isinstance(opt_state, (jnp.ndarray, np.ndarray)):
-        wandb.log({f"Optimizer State/{prefix}": wandb.Histogram(opt_state.flatten(), num_bins=100)}, step=epoch)
-
+def log_optimizer_state(opt_state, params, epoch, prefix=''):
+        index_to_name = {1: 'first_momentum', 2: 'second_momentum'}
+        
+        if isinstance(opt_state, dict):
+            for k, v in opt_state.items():
+                new_prefix = f"{prefix}/{k}" if prefix else k
+                log_optimizer_state(v, params.get(k, {}), new_prefix)
+        elif isinstance(opt_state, (list, tuple)):
+            for idx, v in enumerate(opt_state):
+                if idx in index_to_name:
+                    name = index_to_name[idx]
+                    new_prefix = f"{prefix}/{name}" if prefix else name
+                    log_optimizer_state(v, params, new_prefix)
+                else:
+                    log_optimizer_state(v, params, prefix)
+        elif isinstance(opt_state, (jnp.ndarray, np.ndarray)):
+            wandb.log({f"Optimizer State/{prefix}": wandb.Histogram(opt_state.flatten(), num_bins=100)}, step=epoch)
 
 def save_checkpoint(ckpt_path, state, epoch):
     with open(ckpt_path, "wb") as outfile:
@@ -70,7 +58,7 @@ def save_checkpoint(ckpt_path, state, epoch):
     print(f"Uploading checkpoint to {ckpt_path}")
     artifact.add_reference(f"file://{ckpt_path}")
 
-    wandb.log_artifact(artifact, aliases=[f"epoch_{epoch}"])
+    wandb.log_artifact(artifact, aliases=[f"epoch_{epoch}", "latest"])
 
 
 def load_checkpoint(ckpt_file, state, epoch):
@@ -83,3 +71,5 @@ def load_checkpoint(ckpt_file, state, epoch):
         byte_data = data_file.read()
 
     return from_bytes(state, byte_data)
+
+
