cb48d308f685a3f672e84ca9c69ee9cb5ef95e56
Nikola Drakulic <ndrakulic@tenstorrent.com>
2024-12-24 20:17:34 +0000

    Revert trainer.py

--- thomas/training/pytorch_train/trainer.py
+++ thomas/training/pytorch_train/trainer.py
@@ -16,7 +16,11 @@ class PyTorchTrainer:
 
         self.loss = config.loss()
         self.optimizer = config.optimizer(self.model.parameters(), **config.optimizer_kwargs)
-        self.lr_scheduler = None
+        self.lr_scheduler = get_linear_schedule_with_warmup(
+            optimizer=self.optimizer,
+            num_warmup_steps=0,
+            num_training_steps=(len(self.train_dataloader) * config.epochs),
+        )
         self.epochs = config.epochs
         self.start_epoch = 1
         self.global_step = 0
@@ -25,32 +29,28 @@ class PyTorchTrainer:
 
         self.logger = logger
 
-    def _train_batch(self, batch):
-        x, y = batch
-        pred = self.model(x)
-        loss = self.loss(pred, y)
-        return loss
-
-    def _prepare_batch(self, batch):
-        return batch
-
     def _train_epoch(self):
         self.model.train()
 
         epoch_loss = 0
         for _, batch in enumerate(tqdm(self.train_dataloader)):
             self.global_step += 1
-            batch = self._prepare_batch(batch)
+            batch = {k: v.to(self.run_on) for k, v in batch.items()}
 
             self.optimizer.zero_grad()
-            loss = self._train_batch(batch)
 
+            logits = self.model(batch["input_ids"])
+            # Hack for HF models, TorchTune returns logits directly
+            if isinstance(self.model, PeftModel):
+                logits = logits.logits
+
+            loss = self.loss(logits.flatten(0, 1), batch["labels"].flatten())
             epoch_loss += loss.item()
-            self.logger.log_metrics({"batch_loss": loss.item()}, step=self.global_step)
-            self.logger.save_checkpoint_step(self.model, self.optimizer, self.global_step)
-
             loss.backward()
             self.optimizer.step()
+
+            self.logger.log_metrics({"batch_loss": loss.item()}, step=self.global_step)
+            self.logger.save_checkpoint_step(self.model, self.optimizer, self.global_step)
 
         epoch_loss /= len(self.train_dataloader)
 
@@ -82,33 +82,8 @@ class PyTorchTrainer:
 
     def train(self):
         self.logger.watch_model(self.model, log="all", log_freq=self.logger.config.log_every_n_steps)
-
         for epoch in range(self.start_epoch, self.epochs + 1):
             epoch_loss, validation_loss = self._train_epoch()
 
             self.logger.log_metrics({"epoch_loss": epoch_loss, "epoch": epoch})
             self.logger.save_checkpoint_epoch(self.model, self.optimizer, epoch, epoch_loss, validation_loss)
-
-
-class LoraTrainer(PyTorchTrainer):
-    def __init__(self, model, train_dataloader, valid_dataloader, config, logger):
-        self.lr_scheduler = get_linear_schedule_with_warmup(
-            optimizer=self.optimizer,
-            num_warmup_steps=0,
-            num_training_steps=(len(self.train_dataloader) * config.epochs),
-        )
-        super().__init__(model, train_dataloader, valid_dataloader, config, logger)
-
-    def _prepare_batch(self, batch):
-        run_on = self.run_on
-        # HACK: TT doesn't support moving to "tt" device
-        if self.run_on == "tt":
-            run_on = "cpu"
-
-        batch = {k: v.to(run_on) for k, v in batch.items()}
-        return batch
-
-    def _train_batch(self, batch):
-        x, y = batch["input_ids"], batch["labels"]
-        pred = self.model(x, labels=y)
-        return pred.loss
