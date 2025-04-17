953b107466c1b86cd1acc244361b8ce4f6c34135
Uros Males <umales@tenstorrent.com>
2025-04-17 10:34:44 +0200

    Refactor and add Jax MNIST training to tt-blacksmith. (#147)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-blacksmith/issues/95)
    
    ### Problem description
    Transferring JAX/Flax code for MNIST from old tt-thomas to new
    tt-blacksmith repo.
    
    ### What's changed
    Additional refactoring.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

---
+++ thomas/experiments/jax/mnist/configs.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+from pydantic import BaseModel, Field
+from thomas.experiments.jax.mnist.logging.logger_config import LoggerConfig, get_default_logger_config
+
+
+class NetConfig(BaseModel):
+    input_size: int
+    output_size: int
+
+
+class MNISTLinearConfig(NetConfig):
+    input_size: int = 784
+    output_size: int = 10
+    hidden_size: int = 128
+
+
+class TrainingConfig(BaseModel):
+    batch_size: int = 128
+    epochs: int = 10
+    lr: float = 0.001
+    run_test: bool = True
+    export_shlo: bool = False
+
+
+class ExperimentConfig(BaseModel):
+    net_config: MNISTLinearConfig
+    training_config: TrainingConfig
+    logger_config: LoggerConfig = Field(default_factory=get_default_logger_config)
---
+++ thomas/models/jax/mnist/model.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import flax.linen as nn
+
+
+class MLP(nn.Module):
+    hidden_size: int = 128
+
+    @nn.compact
+    def __call__(self, x):
+        x = x.reshape((x.shape[0], -1))
+        x = nn.Dense(features=self.hidden_size)(x)
+        x = nn.Dense(features=self.hidden_size)(x)
+        x = nn.Dense(features=10)(x)
+        return x
+
+
+class Models:
+    def __init__(self, model_type="MLP", hidden_size=128):
+        if model_type == "MLP":
+            self.model = MLP(hidden_size=hidden_size)
+        else:
+            raise ValueError(f"Unknown model type: {model_type}")
+
+    def __call__(self, x):
+        return self.model(x)
