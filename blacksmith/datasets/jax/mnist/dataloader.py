7c8b8182d40fbd260a3a8b7acef92ef515d5285d
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-04-25 10:12:29 +0000

    Split dataset to frameworks

--- blacksmith/datasets/jax/mnist/dataloader.py
+++ blacksmith/datasets/jax/mnist/dataloader.py
@@ -7,51 +7,11 @@ from torchvision.datasets import MNIST as mnist_dataset
 from torch.utils.data import DataLoader
 from torch.nn import functional as F
 import torch
-
-
-def load_mnist_torch(dtype, batch_size):
-    dtype = dtype
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,)),  # Mean and std for MNIST
-            transforms.Lambda(lambda x: x.view(-1)),  # Flatten image
-            transforms.Lambda(lambda x: x.to(dtype)),  # Convert to dtype
-        ],
-    )
-    target_transform = transforms.Compose(
-        [
-            transforms.Lambda(lambda y: torch.tensor(y, dtype=torch.long)),
-            transforms.Lambda(lambda y: F.one_hot(y, num_classes=10).to(dtype)),
-        ]
-    )
-
-    train_dataset = mnist_dataset(
-        root="data",
-        train=True,
-        download=True,
-        transform=transform,
-        target_transform=target_transform,
-    )
-
-    test_dataset = mnist_dataset(
-        root="data",
-        train=False,
-        download=True,
-        transform=transform,
-        target_transform=target_transform,
-    )
-
-    # Drop last to ensure all batches are the same size as compiled model expects
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
-
-    return train_loader, test_loader
+import jax
+import jax.numpy as jnp
 
 
 def load_mnist_jax():
-    import jax
-    import jax.numpy as jnp
     # Load MNIST data using torchvision
     mnist = {
         "train": torchvision.datasets.MNIST("./data", train=True, download=True),
