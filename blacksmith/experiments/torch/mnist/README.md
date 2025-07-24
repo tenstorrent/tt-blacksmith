9494eaf4e9437ce48753d64e3bcd090fb68ddd00
abogdanovicTT <abogdanovic@tenstorrent.com>
2025-07-24 13:45:43 +0000

    Test MNIST training

--- blacksmith/experiments/torch/mnist/README.md
+++ blacksmith/experiments/torch/mnist/README.md
@@ -1,6 +1,7 @@
 # MNIST Linear model training experiment
 
 This directory contains the code for the training linear model for MNIST dataset, using torch_xla environment.
+Linear model is from [tt-blacksmith/blacksmith/models/torch/mnist/mnist_linear.py]
 
 ## Environment
 
@@ -8,10 +9,10 @@ Activate tt-xla environment, install and build tt-xla following instructions fro
 
 ## Training
 
-Test MNIST training running [mnist_train.py].
+Test MNIST training running [test_mnist_training.py].
 
 ```bash
-python blacksmith/experiments/torch/mnist/mnist_train.py
+python blacksmith/experiments/torch/mnist/test_mnist_training.py
 ```
 
 
---
+++ blacksmith/experiments/torch/mnist/configs.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import List
+from pydantic import BaseModel, Field
+
+from blacksmith.tools.logging.configs import LoggerConfig, get_default_logger_config
+
+
+class MNISTLinearConfig(BaseModel):
+    input_size: int = 784
+    hidden_size: int = 512
+    output_size: int = 10
+    bias: bool = True
+
+
+class TrainingConfig(BaseModel):
+    batch_size: int = 64
+    epochs: int = 5
+    lr: float = 0.001
+
+
+class DataLoadingConfig(BaseModel):
+    batch_size: int = 64
+    dtype: str = "torch.bfloat16"
+
+
+class ExperimentConfig(BaseModel):
+    experiment_name: str = "blacksmith-mnist"
+    tags: List[str] = ["tt-xla", "model:torch", "plugin", "wandb"]
+    net_config: MNISTLinearConfig = Field(default_factory=MNISTLinearConfig)
+    loss: str = "torch.nn.MSELoss"
+    training_config: TrainingConfig = Field(default_factory=TrainingConfig)
+    data_loading_config: DataLoadingConfig = Field(default_factory=DataLoadingConfig)
+    logger_config: LoggerConfig = Field(default_factory=get_default_logger_config)
