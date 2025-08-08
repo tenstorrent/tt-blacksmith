60e3d5716d7ed46ccd6dec7f615a658ff1560610
Pavle Glusac <pglusac@tenstorrent.com>
2025-08-08 21:21:07 +0000

    Make llama lora work fully on device

---
+++ blacksmith/experiments/torch/llama/loss.py
@@ -0,0 +1,38 @@
+import torch
+from forge.module import ForgeModule
+from forge.op.reduce import ReduceAvg, ReduceSum
+from forge.op.eltwise_unary import Log, Cast
+from forge.op.eltwise_binary import Multiply
+from forge.op.nn import Softmax
+from forge.op.constant import Constant
+
+# Due to softmax issues, we are using a custom loss function
+# This softmax uses dim=0 instead of dim=-1
+class CrossEntropyLoss(ForgeModule):
+    """
+    Cross-Entropy Loss (with mean reduction)
+
+    loss = reduce_avg(-1 * sum(labels * log(softmax(predictions)), dim=-1), dim=0)
+    """
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.is_loss = True
+
+    def forward(self, predictions, labels):
+        softmax = Softmax("softmax", predictions, dim=0)
+        log_softmax = Log("log", softmax)
+
+        product = Multiply("products", labels, log_softmax)
+        log_loss = ReduceSum("log_loss", product, dim=0)
+
+        negative_one_constant = Constant("negative_one_const", constant=-1.0)
+        negative_one_constant = Cast("negative_one_cast", negative_one_constant, dtype=torch.bfloat16)
+        negative_log_loss = Multiply(
+            "negative_log_loss",
+            log_loss,
+            negative_one_constant,
+        )
+
+        reduction_mean = ReduceAvg("reduction_mean", negative_log_loss, dim=-1)
+        return reduction_mean
