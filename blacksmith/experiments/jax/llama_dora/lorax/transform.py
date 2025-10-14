41dbc997ac174f31482756c8721b59851cdfc06c
Uros Pantelic <upantelic@tenstorrent.com>
2025-10-14 13:44:17 +0000

    changed if statement bug

--- blacksmith/experiments/jax/llama_dora/lorax/transform.py
+++ blacksmith/experiments/jax/llama_dora/lorax/transform.py
@@ -96,7 +96,7 @@ def eval_dora_transpose(arg: DoraWeight, *, permutation: Any) -> Any:
     """
     Define how a `DoraWeight` behaves under transpose.
     """
-    if not len(arg.shape) == 2 and permutation == (1, 0):
+    if not (len(arg.shape) == 2 and permutation == (1, 0)):
         return NotImplemented
 
     return DoraWeight(
