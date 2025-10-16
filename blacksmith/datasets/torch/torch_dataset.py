f30d3cfd9be361de48fdc0e88c64c4053fa6f8a8
Pavle Markovic <pmarkovic@tenstorrent.com>
2025-10-16 15:27:47 +0000

    Update experiment template

--- blacksmith/datasets/torch/torch_dataset.py
+++ blacksmith/datasets/torch/torch_dataset.py
@@ -11,7 +11,7 @@ from blacksmith.experiments.torch.llama.configs import TrainingConfig
 
 class BaseDataset(Dataset, ABC):
     """Abstract base class for all PyTorch dataset implementations"""
-    
+
     def __init__(self, config: TrainingConfig, split: str = "train"):
         """
         Args:
@@ -20,22 +20,22 @@ class BaseDataset(Dataset, ABC):
         """
         self.config = config
         self.split = split
-        
+
         self._prepare_dataset(split)
-    
+
     @abstractmethod
     def _prepare_dataset(self, split: str):
         """Load and prepare the dataset"""
         pass
-    
+
     def __len__(self) -> int:
         """Return the number of examples in the dataset"""
         return len(self.dataset)
-    
+
     def __getitem__(self, idx: int) -> Dict:
         """Get a single example from the dataset"""
         pass
-    
+
     @abstractmethod
     def get_dataloader(self) -> DataLoader:
         """Create and return a DataLoader for this dataset"""
