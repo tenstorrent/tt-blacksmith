1715d05665d9a5a1c709075c178adbc34d496c20
Milan Cupac <mcupac@tenstorrent.com>
2025-11-13 14:47:31 +0000

    temporary change gemma finetuning py for new dataset

--- blacksmith/experiments/torch/gemma/test_gemma_finetuning.py
+++ blacksmith/experiments/torch/gemma/test_gemma_finetuning.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 from transformers import PreTrainedTokenizer
 
 from blacksmith.experiments.torch.gemma.configs import TrainingConfig
-from blacksmith.datasets.torch.sst2.sst2_dataset import SSTDataset
+from blacksmith.datasets.torch.squadV2.squadV2_dataset import SquadV2Dataset
 from blacksmith.models.torch.huggingface.hf_models import get_model
 from blacksmith.tools.cli import generate_config
 from blacksmith.tools.reproducibility_manager import ReproducibilityManager
@@ -96,11 +96,11 @@ def train(
         checkpoint_manager.load_checkpoint()
 
     # Load dataset
-    train_dataset = SSTDataset(config=config, collate_fn=collate_fn_for_causal_lm)
+    train_dataset = SquadV2Dataset(config=config, collate_fn=collate_fn_for_causal_lm)
     train_dataloader = train_dataset.get_dataloader()
     logger.info(f"Loaded {config.dataset_id} dataset. Train dataset size: {len(train_dataloader)*config.batch_size}")
 
-    eval_dataset = SSTDataset(config=config, split="validation", collate_fn=collate_fn_for_causal_lm)
+    eval_dataset = SquadV2Dataset(config=config, split="validation", collate_fn=collate_fn_for_causal_lm)
     eval_dataloader = eval_dataset.get_dataloader()
     logger.info(f"Loaded {config.dataset_id} dataset. Eval dataset size: {len(eval_dataloader)*config.batch_size}")
 
