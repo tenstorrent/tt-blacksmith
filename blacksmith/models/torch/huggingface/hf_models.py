a7147195d04a1c5de55d9b58d2db2c083fd979fd
Pavle Markovic <pmarkovic@tenstorrent.com>
2025-11-14 14:55:38 +0000

    tt test

--- blacksmith/models/torch/huggingface/hf_models.py
+++ blacksmith/models/torch/huggingface/hf_models.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import torch
 import torch.nn as nn
-from transformers import AutoModelForCausalLM, AutoModelForSequenceClassification, AlbertModel
+from transformers import AutoModelForCausalLM, AlbertModel
 from peft import LoraConfig, get_peft_model
 
 from blacksmith.experiments.torch.llama.configs import TrainingConfig
@@ -41,18 +41,18 @@ class AlbertWithMLP(nn.Module):
     def __init__(self, config: TrainingConfig):
         super().__init__()
         self.encoder = AlbertModel.from_pretrained(config.model_name)
-        
+
         # Freeze encoder
         for param in self.encoder.parameters():
             param.requires_grad = False
-        
+
         # Simple MLP - NO dropout
         self.classifier = nn.Sequential(
             nn.Linear(768, config.mlp_hidden_dim),
             nn.GELU(),
             nn.Linear(config.mlp_hidden_dim, config.num_labels)
         )
-    
+
     def forward(self, input_ids, attention_mask=None, token_type_ids=None):
         outputs = self.encoder(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         cls_output = outputs.last_hidden_state[:, 0, :]
