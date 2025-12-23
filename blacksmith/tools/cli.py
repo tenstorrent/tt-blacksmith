201557160a9d81c58e041ccecaa42f10a8f3ca3d
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-12-23 12:14:57 +0000

    pre-commit

--- blacksmith/tools/cli.py
+++ blacksmith/tools/cli.py
@@ -14,10 +14,13 @@ def generate_config(config: BaseModel, yaml_path: Path) -> BaseModel:
         data = yaml.safe_load(file)
     return config.model_validate(data)
 
+
 def parse_cli_options(default_config: Path) -> argparse.Namespace:
     parser = argparse.ArgumentParser("Experiment CLI", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
-    parser.add_argument("--config", type=Path, default=default_config.relative_to(Path.cwd()), help="Path to YAML config file")
-    
+
+    parser.add_argument(
+        "--config", type=Path, default=default_config.relative_to(Path.cwd()), help="Path to YAML config file"
+    )
+
     args = parser.parse_args()
     return args
