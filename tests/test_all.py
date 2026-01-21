4ed669209640ddeb061f92c725268f0325e023e6
agobeljicTT <agobeljic@tenstorrent.com>
2026-01-21 13:51:26 +0000

    migration

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,30 @@ import subprocess
 from pathlib import Path
 import sys
 import os
+import pandas as pd
 from training_test_cases import TRAINING_TEST_CASES
+
+def assert_wandb_history_equals_with_tolerance(
+    history,
+    golden_history,
+    tolerances={
+        "_runtime": 0.1,
+        "val/loss": 0.002,
+        "val/accuracy": 0.002,
+        "train/loss": 0.002,
+    }
+):
+    """
+    Assert that the history is equal to the golden history with a tolerance.
+    """
+    for col, tolerance in tolerances.items():
+        diff = (history[col] - golden_history[col]).abs()
+        ok = (diff <= tolerance).all()
+
+        if not ok:
+            # TODO: (agobeljicTT) Add a more detailed explanation of the failure.
+            pytest.fail(f"Wandb history is not equal to the golden history with a tolerance of {tolerance} for column {col}")
+
 
 @pytest.mark.parametrize(
     "test_script,test_config,timeout",
@@ -19,6 +42,7 @@ def test_training_script(test_script, test_config, timeout, request):
     """
 
     test_id = request.node.callspec.id
+    golden_csv_path = Path(f"tests/golden_files/{test_id}.csv")
 
     # Verify files exist
     assert Path(test_script).exists(), f"Script not found: {test_script}"
@@ -33,9 +57,6 @@ def test_training_script(test_script, test_config, timeout, request):
     ]
 
     try:
-        env = os.environ.copy()
-        env["WANDB_MODE"] = "dryrun"
-
         result = subprocess.run(
             cmd,
             cwd=str(Path.cwd()),
@@ -43,7 +64,6 @@ def test_training_script(test_script, test_config, timeout, request):
             capture_output=True,
             text=True,
             check=False,
-            env=env
         )
 
         # Check exit code
@@ -56,10 +76,22 @@ def test_training_script(test_script, test_config, timeout, request):
             print(f"{'='*60}\n")
             pytest.fail(f"Training script exited with code {result.returncode}")
 
-        api = wandb.Api()
-        run = api.runs("test-all-wandb-project")[0]
-        history = run.history()
-        print(f"RANKO: {history}")
+        # Get the last run from the project
+        #api = wandb.Api()
+        #runs = api.runs("test-all-wandb-project") # Universally unique identifier
+        #run = runs[len(runs) - 1]
+        #history = run.history()
+
+        # TODO: (agobeljicTT) Add golden file functionality.
+        # If the golden CSV file exists, compare the history to the golden CSV file
+        #if golden_csv_path.exists():
+        #    golden_history = pd.read_csv(golden_csv_path)
+        #    assert_wandb_history_equals_with_tolerance(history, golden_history)
+        #
+        # If the golden CSV file does not exist, save the history to the golden CSV file
+        #else:
+        #    history.to_csv(golden_csv_path, index=False)
+        #    pytest.skip(f"Golden CSV file not found, generated new one: {golden_csv_path}. Please rerun the test.")
 
     except subprocess.TimeoutExpired:
         pytest.fail(f"Training script timed out after {timeout} seconds")
