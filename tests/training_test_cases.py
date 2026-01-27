74def9ead4e052ebcc83b6e04dc47aa3034d6bb0
agobeljicTT <agobeljic@tenstorrent.com>
2026-01-27 15:34:29 +0000

    black

--- tests/training_test_cases.py
+++ tests/training_test_cases.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
-import pytest
-
 from typing import NewType
+
+import pytest
 
 Seconds = NewType("Seconds", int)
 
