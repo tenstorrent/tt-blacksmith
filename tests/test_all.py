93bb078afdb5966597f4bcaa4d435df951b1c6e5
agobeljicTT <agobeljic@tenstorrent.com>
2026-01-30 15:06:09 +0000

    remove wandb

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,39 +5,8 @@ import subprocess
 import sys
 from pathlib import Path
 
-import google.protobuf.message
-import pandas as pd
 import pytest
-from wandb.proto import wandb_internal_pb2
-from wandb.sdk.internal import datastore
 from training_test_cases import TRAINING_TEST_CASES
-
-
-def get_history_from_wandb_file(wandb_file: Path) -> pd.DataFrame:
-    ds = datastore.DataStore()
-    ds.open_for_scan(str(wandb_file))
-
-    train_loss = []
-    val_loss = []
-    _step = []
-
-    for record_bytes in iter(lambda: ds.scan_record(), None):
-        try:
-            pb = wandb_internal_pb2.Record()
-            pb.ParseFromString(record_bytes[1])
-            if pb.HasField("history"):
-                for item in pb.history.item:
-                    if item.nested_key[0] == "train/loss":
-                        train_loss.append(float(item.value_json))
-                    elif item.nested_key[0] == "val/loss":
-                        val_loss.append(float(item.value_json))
-                    elif item.nested_key[0] == "_step":
-                        _step.append(int(item.value_json))
-
-        except google.protobuf.message.DecodeError as e:
-            print(f"Error decoding record: \n{e}\n")
-
-    return pd.DataFrame({"_step": _step, "train/loss": train_loss, "val/loss": val_loss})
 
 
 @pytest.mark.parametrize("setup_dict", TRAINING_TEST_CASES)
@@ -101,21 +70,3 @@ def test_training_script(
 
     except subprocess.TimeoutExpired:
         pytest.fail(f"Training script timed out after {setup_dict['timeout']} seconds")
-
-
-    wandb_dir = Path("./wandb/latest-run")
-    wandb_files = list(wandb_dir.glob("*.wandb"))
-    assert len(wandb_files) > 0, "No .wandb files found in latest-run directory"
-    wandb_file = wandb_files[0]
-
-    history = get_history_from_wandb_file(wandb_file)
-    golden_file = Path(f"tests/golden_files/{test_id}.csv")
-
-    if golden_file.exists():
-        # Golden file already exists, so we can compare the history to it.
-        golden_history = pd.read_csv(golden_file, index_col=0)
-        pd.testing.assert_frame_equal(history, golden_history, rtol=setup_dict["tolerance"])
-    # else:
-    # Golden file doesn't exist, so we can save the history to it.
-    # TODO: Uncomment this when on GPU.
-    # history.to_csv(golden_file)
