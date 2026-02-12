b1849088b7ddb0237cd47a72485b399d7ba0297b
agobeljicTT <agobeljic@tenstorrent.com>
2026-02-12 14:15:48 +0000

    Add notifications

---
+++ tests/error_messages.py
@@ -0,0 +1,27 @@
+TOLERANCE_CHECK_WARNING_MESSAGE = """
+=====================TOLERANCE CHECK WARNING====================
+Test ID: {test_id}
+Error: {error}
+=====================TOLERANCE CHECK WARNING====================
+"""[1:]
+
+CORRECTNESS_FAILURE_MESSAGE = """
+=====================CORRECTNESS FAILURE====================
+Test ID: {test_id}
+Error: {error}
+=====================CORRECTNESS FAILURE====================
+"""[1:]
+
+TIMED_OUT_MESSAGE = """
+=====================TIMED OUT====================
+Test ID: {test_id}
+Timeout: {timeout} seconds
+=====================TIMED OUT====================
+"""[1:]
+
+RUN_ERROR_MESSAGE = """
+=====================RUN ERROR====================
+Test ID: {test_id}
+Error: {error}
+=====================RUN ERROR====================
+"""[1:]
