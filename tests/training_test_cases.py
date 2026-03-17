1e5ba79bd16ba7b96de4e48c708a1e349630b4f7
agobeljicTT <agobeljic@tenstorrent.com>
2026-03-17 14:10:25 +0000

    black

--- tests/training_test_cases.py
+++ tests/training_test_cases.py
@@ -127,7 +127,8 @@ TRAINING_TEST_CASES = [
                 pytest.mark.split_0,
             ],
             id=f"tt-llama_3_2_1b-sst2-n150-{i}",
-        ) for i, test_config in enumerate(
+        )
+        for i, test_config in enumerate(
             [
                 "tests/configs/tt-llama_3_2_1b-sst2-n150.yaml",
                 "tests/configs/tt-llama_3_2_1b-sst2-n150_checkpoint_step1340.yaml",
