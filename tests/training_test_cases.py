bbe25ace9cc20b3fcd7ec1f2ee6238c1e1403465
agobeljicTT <agobeljic@tenstorrent.com>
2026-04-21 13:18:59 +0000

    rename

--- tests/training_test_cases.py
+++ tests/training_test_cases.py
@@ -182,8 +182,8 @@ TRAINING_TEST_CASES = [
     ),
     pytest.param(
         {
-            "test_script": "blacksmith/experiments/torch/llama/xla/test_llama_fine_tuning_pure_torch.py",
-            "experiment_config": "blacksmith/experiments/torch/llama/xla/lora/quietbox/test_llama_3_1_8b.yaml",
+            "test_script": "blacksmith/experiments/torch/llama/xla/train.py",
+            "experiment_config": "blacksmith/experiments/torch/llama/xla/lora/quietbox/llama_3_1_8b_sst2.yaml",
             "test_config": "tests/configs/tt-llama_3_1_8b-sst2-qb2-blackhole.yaml",
             "timeout": 5000,
         },
@@ -197,8 +197,8 @@ TRAINING_TEST_CASES = [
     ),
     pytest.param(
         {
-            "test_script": "blacksmith/experiments/torch/llama/xla/test_llama_fine_tuning_pure_torch.py",
-            "experiment_config": "blacksmith/experiments/torch/llama/xla/lora/quietbox/test_llama_3_1_8b_sst2_1x8.yaml",
+            "test_script": "blacksmith/experiments/torch/llama/xla/train.py",
+            "experiment_config": "blacksmith/experiments/torch/llama/xla/lora/quietbox/llama_3_1_8b_sst2_1x8.yaml",
             "timeout": 5000,
         },
         marks=[
