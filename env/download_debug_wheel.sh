eab497217d1a406d01352c46d1391768a8901363
Nikola Drakulic <ndrakulic@tenstorrent.com>
2026-05-14 14:22:27 +0000

    fix year

--- env/download_debug_wheel.sh
+++ env/download_debug_wheel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
 
 # Downloads and installs the debug PJRT plugin wheel from GitHub Actions,
