e9d7a13f07f7a67405c9edd9bb55ccc88f23afb6
umalesTT <umales@tenstorrent.com>
2026-05-20 13:47:00 +0000

    [WIP] Gemma4 E2B

--- blacksmith/datasets/torch/dataset_utils.py
+++ blacksmith/datasets/torch/dataset_utils.py
@@ -19,6 +19,7 @@ from blacksmith.datasets.torch.stanfordcars.stanfordcars_dataset import (
     StanfordCarsDataset,
 )
 from blacksmith.datasets.torch.text2sql.text2sql_dataset import TextToSQLDataset
+from blacksmith.datasets.torch.wizardlm_evol.wizardlm_evol_dataset import WizardLMEvolDataset
 from blacksmith.tools.templates.configs import TrainingConfig
 
 
@@ -36,6 +37,7 @@ class AvailableDataset(Enum):
     FUSECHAT = "fusechat"
     ALPACA = "alpaca"
     METAMATHQA = "metamathqa"
+    WIZARDLM_EVOL = "wizardlm_evol"
 
 
 def get_dataset(config: TrainingConfig, split: str = "train", collate_fn=None):
@@ -71,6 +73,8 @@ def get_dataset(config: TrainingConfig, split: str = "train", collate_fn=None):
         return MathDPODataset(config, split, collate_fn=collate_fn)
     elif dataset_id == AvailableDataset.MATH_PREFERENCE_SFT.value:
         return MathSFTDataset(config, split, collate_fn=collate_fn)
+    elif dataset_id == AvailableDataset.WIZARDLM_EVOL.value:
+        return WizardLMEvolDataset(config, split, collate_fn=collate_fn)
     else:
         available_datasets = [ds.value for ds in AvailableDataset]
         raise ValueError(f"Unsupported dataset: {dataset_id}. Available options are: {available_datasets}")
