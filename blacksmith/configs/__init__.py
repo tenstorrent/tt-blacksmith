222857212d0a6d90a36b8eba10365f92f10d608f
agobeljicTT <agobeljic@tenstorrent.com>
2026-09-24 17:06:55 +0200

    leftovers

--- blacksmith/configs/__init__.py
+++ blacksmith/configs/__init__.py
@@ -3,6 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 from blacksmith.configs.checkpoint import CheckpointConfig
 from blacksmith.configs.dataset import CustomDatasetConfig
+from blacksmith.configs.logger import (
+    CheckpointLoggerConfig,
+    LoggerConfig,
+    get_default_logger_config,
+)
 from blacksmith.configs.logging import LoggingConfig
 from blacksmith.configs.lora_llm import LoraLLMConfig
 from blacksmith.configs.metrics import MetricsConfig
@@ -12,12 +17,15 @@ from blacksmith.configs.training import Framework, TrainingConfig
 
 __all__ = [
     "CheckpointConfig",
+    "CheckpointLoggerConfig",
     "CustomDatasetConfig",
     "Framework",
+    "LoggerConfig",
     "LoggingConfig",
     "LoraLLMConfig",
     "MetricsConfig",
     "TestConfig",
     "TrainerConfig",
     "TrainingConfig",
+    "get_default_logger_config",
 ]
